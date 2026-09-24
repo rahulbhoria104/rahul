32644e3b3ca5501e1cfa6aeb0d831d6df22774a4
rahulbhoria104 <rahulbhoria104@gmail.com>
2026-09-24 14:09:35 +0530

    rahul

--- wp-config.php
+++ wp-config.php
@@ -92,6 +92,7 @@ define( 'WP_DEBUG', false );
 
 
 /* That's all, stop editing! Happy publishing. */
+set_time_limit(1200);
 
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
